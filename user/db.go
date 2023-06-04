5b5b04c9ff52e9420c477cad407513a6b25e6c62
Liam Burnand <liamb1216@gmail.com>
2023-06-04 06:53:48 +0100

    Yes, just yes... a lot of searching, sorting and other things

--- user/db.go
+++ user/db.go
@@ -3,7 +3,6 @@ package user
 import (
 	"context"
 	"fmt"
-	"github.com/ystv/web-auth/permission"
 	"github.com/ystv/web-auth/role"
 	"time"
 )
@@ -71,21 +70,21 @@ func (s *Store) updateUser(ctx context.Context, user User) error {
 func (s *Store) getUser(ctx context.Context, user User) (User, error) {
 	u := User{}
 	err := s.db.GetContext(ctx, &u,
-		`SELECT user_id, username, nickname, first_name, last_name, email, last_login, salt, password, avatar, use_gravatar
+		`SELECT *
 		FROM people.users
-		WHERE (username = $1 AND username != '') OR (email = $2 AND email != '') OR user_id = $3
-		LIMIT 1;`, user.Username, user.Email, user.UserID)
+		WHERE (username = $1 AND username != '') OR (email = $2 AND email != '') OR (ldap_username = $3 AND ldap_username != '') OR user_id = $4
+		LIMIT 1;`, user.Username, user.Email, user.LDAPUsername, user.UserID)
 	if err != nil {
 		return u, fmt.Errorf("failed to get user from db: %w", err)
 	}
 	return u, nil
 }
 
-// getUsers will get a group of users
+// getUsers will get users
 func (s *Store) getUsers(ctx context.Context) ([]User, error) {
 	var u []User
 	err := s.db.SelectContext(ctx, &u,
-		`SELECT user_id, username, nickname, first_name, last_name, email, last_login, avatar, use_gravatar
+		`SELECT *
 		FROM people.users;`)
 	if err != nil {
 		return nil, err
@@ -93,130 +92,245 @@ func (s *Store) getUsers(ctx context.Context) ([]User, error) {
 	return u, nil
 }
 
-// getUsersIDA will get a group of users by userId ASC
-func (s *Store) getUsersIDA(ctx context.Context) ([]User, error) {
+// getUsersSizePage will get users with page size
+func (s *Store) getUsersSizePage(ctx context.Context, size, page int) ([]User, error) {
 	var u []User
+	// SELECT u.*, COUNT(*) / $1 AS pages
 	err := s.db.SelectContext(ctx, &u,
-		`SELECT user_id, username, nickname, first_name, last_name, email, last_login, avatar, use_gravatar
-		FROM people.users
-		ORDER BY user_id ASC;`)
+		`SELECT u.*
+		FROM people.users u
+-- 		GROUP BY u, user_id, username, university_username, email, first_name, last_name, nickname, login_type, password, salt, avatar, last_login, reset_pw, enabled, created_at, created_by, updated_at, updated_by, deleted_at, deleted_by, use_gravatar, ldap_username
+		LIMIT $1
+		OFFSET $2;`, size, size*(page-1))
 	if err != nil {
 		return nil, err
 	}
 	return u, nil
 }
 
-// getUsersIDD will get a group of users by userId DESC
-func (s *Store) getUsersIDD(ctx context.Context) ([]User, error) {
+// getUsersSearch will get users search
+func (s *Store) getUsersSearch(ctx context.Context, search string) ([]User, error) {
 	var u []User
 	err := s.db.SelectContext(ctx, &u,
-		`SELECT user_id, username, nickname, first_name, last_name, email, last_login, avatar, use_gravatar
+		`SELECT *
 		FROM people.users
-		ORDER BY user_id DESC;`)
+		WHERE (CAST(user_id AS TEXT) LIKE '%' || $1 || '%')
+		   OR (username LIKE '%' || $1 || '%')
+		   OR (nickname LIKE '%' || $1 || '%')
+		   OR (first_name LIKE '%' || $1 || '%')
+		   or (last_name LIKE '%' || $1 || '%')
+		   OR (email LIKE '%' || $1 || '%')
+		   OR (first_name || ' ' || last_name LIKE '%' || $1 || '%');`, search)
 	if err != nil {
 		return nil, err
 	}
 	return u, nil
 }
 
-// getUsersFLNA will get a group of users by first and last name ASC
-func (s *Store) getUsersFLNA(ctx context.Context) ([]User, error) {
+// getUsersSearchSizePage will get users search with size and page
+func (s *Store) getUsersSearchSizePage(ctx context.Context, search string, size, page int) ([]User, error) {
 	var u []User
 	err := s.db.SelectContext(ctx, &u,
-		`SELECT user_id, username, nickname, first_name, last_name, email, last_login, avatar, use_gravatar
+		`SELECT *
 		FROM people.users
-		ORDER BY first_name ASC, last_name ASC;`)
+		WHERE (CAST(user_id AS TEXT) LIKE '%' || $1 || '%')
+		   OR (username LIKE '%' || $1 || '%')
+		   OR (nickname LIKE '%' || $1 || '%')
+		   OR (first_name LIKE '%' || $1 || '%')
+		   or (last_name LIKE '%' || $1 || '%')
+		   OR (email LIKE '%' || $1 || '%')
+		   OR (first_name || ' ' || last_name LIKE '%' || $1 || '%')
+-- 		GROUP BY u, user_id, username, university_username, email, first_name, last_name, nickname, login_type, password, salt, avatar, last_login, reset_pw, enabled, created_at, created_by, updated_at, updated_by, deleted_at, deleted_by, use_gravatar, ldap_username
+		LIMIT $2
+		OFFSET $3;`, search, size, size*(page-1))
 	if err != nil {
 		return nil, err
 	}
 	return u, nil
 }
 
-// getUsersIDD will get a group of users by first and last DESC
-func (s *Store) getUsersFLND(ctx context.Context) ([]User, error) {
+// getUsersOptionsDesc will get users sorting asc
+func (s *Store) getUsersOptionsAsc(ctx context.Context, sortBy string) ([]User, error) {
 	var u []User
 	err := s.db.SelectContext(ctx, &u,
-		`SELECT user_id, username, nickname, first_name, last_name, email, last_login, avatar, use_gravatar
+		`SELECT *
 		FROM people.users
-		ORDER BY first_name DESC, last_name DESC;`)
+		ORDER BY
+		    CASE WHEN $1 = 'userId' THEN user_id END ASC,
+		    CASE WHEN $1 = 'name' THEN first_name END ASC,
+			CASE WHEN $1 = 'name' THEN last_name END ASC,
+		    CASE WHEN $1 = 'username' THEN username END ASC,
+		    CASE WHEN $1 = 'email' THEN email END ASC,
+		    CASE WHEN $1 = 'lastLogin' THEN last_login END ASC NULLS FIRST;`, sortBy)
 	if err != nil {
 		return nil, err
 	}
 	return u, nil
 }
 
-// getUsersUA will get a group of users by username ASC
-func (s *Store) getUsersUA(ctx context.Context) ([]User, error) {
+// getUsersOptionsDescSizePage will get users sorting asc with size and page
+func (s *Store) getUsersOptionsAscSizePage(ctx context.Context, sortBy string, size, page int) ([]User, error) {
 	var u []User
 	err := s.db.SelectContext(ctx, &u,
-		`SELECT user_id, username, nickname, first_name, last_name, email, last_login, avatar, use_gravatar
+		`SELECT *
 		FROM people.users
-		ORDER BY username ASC;`)
+		ORDER BY
+		    CASE WHEN $1 = 'userId' THEN user_id END ASC,
+		    CASE WHEN $1 = 'name' THEN first_name END ASC,
+			CASE WHEN $1 = 'name' THEN last_name END ASC,
+		    CASE WHEN $1 = 'username' THEN username END ASC,
+		    CASE WHEN $1 = 'email' THEN email END ASC,
+		    CASE WHEN $1 = 'lastLogin' THEN last_login END ASC NULLS FIRST
+		LIMIT $2
+		OFFSET $3;`, sortBy, size, size*(page-1))
 	if err != nil {
 		return nil, err
 	}
 	return u, nil
 }
 
-// getUsersUD will get a group of users by username DESC
-func (s *Store) getUsersUD(ctx context.Context) ([]User, error) {
+// getUsersSearchOptionsAsc will get users search with sorting asc
+func (s *Store) getUsersSearchOptionsAsc(ctx context.Context, search, sortBy string) ([]User, error) {
 	var u []User
 	err := s.db.SelectContext(ctx, &u,
-		`SELECT user_id, username, nickname, first_name, last_name, email, last_login, avatar, use_gravatar
+		`SELECT *
 		FROM people.users
-		ORDER BY username DESC;`)
+		WHERE (CAST(user_id AS TEXT) LIKE '%' || $1 || '%')
+		   OR (username LIKE '%' || $1 || '%')
+		   OR (nickname LIKE '%' || $1 || '%')
+		   OR (first_name LIKE '%' || $1 || '%')
+		   or (last_name LIKE '%' || $1 || '%')
+		   OR (email LIKE '%' || $1 || '%')
+		   OR (first_name || ' ' || last_name LIKE '%' || $1 || '%')
+		ORDER BY
+		    CASE WHEN $2 = 'userId' THEN user_id END ASC,
+		    CASE WHEN $2 = 'name' THEN first_name END ASC,
+			CASE WHEN $2 = 'name' THEN last_name END ASC,
+		    CASE WHEN $2 = 'username' THEN username END ASC,
+		    CASE WHEN $2 = 'email' THEN email END ASC,
+		    CASE WHEN $2 = 'lastLogin' THEN last_login END ASC NULLS FIRST;`, search, sortBy)
 	if err != nil {
 		return nil, err
 	}
 	return u, nil
 }
 
-// getUsersEA will get a group of users by email ASC
-func (s *Store) getUsersEA(ctx context.Context) ([]User, error) {
+// getUsersSearchOptionsAscSizePage will get users search with sorting asc with size and page
+func (s *Store) getUsersSearchOptionsAscSizePage(ctx context.Context, search, sortBy string, size, page int) ([]User, error) {
 	var u []User
 	err := s.db.SelectContext(ctx, &u,
-		`SELECT user_id, username, nickname, first_name, last_name, email, last_login, avatar, use_gravatar
+		`SELECT *
 		FROM people.users
-		ORDER BY email ASC;`)
+		WHERE (CAST(user_id AS TEXT) LIKE '%' || $1 || '%')
+		   OR (username LIKE '%' || $1 || '%')
+		   OR (nickname LIKE '%' || $1 || '%')
+		   OR (first_name LIKE '%' || $1 || '%')
+		   or (last_name LIKE '%' || $1 || '%')
+		   OR (email LIKE '%' || $1 || '%')
+		   OR (first_name || ' ' || last_name LIKE '%' || $1 || '%')
+		ORDER BY
+		    CASE WHEN $2 = 'userId' THEN user_id END ASC,
+		    CASE WHEN $2 = 'name' THEN first_name END ASC,
+			CASE WHEN $2 = 'name' THEN last_name END ASC,
+		    CASE WHEN $2 = 'username' THEN username END ASC,
+		    CASE WHEN $2 = 'email' THEN email END ASC,
+		    CASE WHEN $2 = 'lastLogin' THEN last_login END ASC NULLS FIRST
+		LIMIT $3
+		OFFSET $4;`, search, sortBy, size, size*(page-1))
 	if err != nil {
 		return nil, err
 	}
 	return u, nil
 }
 
-// getUsersED will get a group of users by email DESC
-func (s *Store) getUsersED(ctx context.Context) ([]User, error) {
+// getUsersOptionsDesc will get users sorting desc
+func (s *Store) getUsersOptionsDesc(ctx context.Context, sortBy string) ([]User, error) {
 	var u []User
 	err := s.db.SelectContext(ctx, &u,
-		`SELECT user_id, username, nickname, first_name, last_name, email, last_login, avatar, use_gravatar
+		`SELECT *
 		FROM people.users
-		ORDER BY email DESC;`)
+		ORDER BY
+		    CASE WHEN $1 = 'userId' THEN user_id END DESC,
+		    CASE WHEN $1 = 'name' THEN first_name END DESC,
+			CASE WHEN $1 = 'name' THEN last_name END DESC,
+		    CASE WHEN $1 = 'username' THEN username END DESC,
+		    CASE WHEN $1 = 'email' THEN email END DESC,
+		    CASE WHEN $1 = 'lastLogin' THEN last_login END DESC NULLS LAST;`, sortBy)
 	if err != nil {
 		return nil, err
 	}
 	return u, nil
 }
 
-// getUsersLLA will get a group of users by lastLogin ASC
-func (s *Store) getUsersLLA(ctx context.Context) ([]User, error) {
+// getUsersOptionsDescSizePage will get users sorting desc with size and page
+func (s *Store) getUsersOptionsDescSizePage(ctx context.Context, sortBy string, size, page int) ([]User, error) {
 	var u []User
 	err := s.db.SelectContext(ctx, &u,
-		`SELECT user_id, username, nickname, first_name, last_name, email, last_login, avatar, use_gravatar
+		`SELECT *
 		FROM people.users
-		ORDER BY last_login ASC NULLS FIRST;`)
+		ORDER BY
+		    CASE WHEN $1 = 'userId' THEN user_id END DESC,
+		    CASE WHEN $1 = 'name' THEN first_name END DESC,
+			CASE WHEN $1 = 'name' THEN last_name END DESC,
+		    CASE WHEN $1 = 'username' THEN username END DESC,
+		    CASE WHEN $1 = 'email' THEN email END DESC,
+		    CASE WHEN $1 = 'lastLogin' THEN last_login END DESC NULLS LAST
+		LIMIT $2
+		OFFSET $3;`, sortBy, size, size*(page-1))
 	if err != nil {
 		return nil, err
 	}
 	return u, nil
 }
 
-// getUsersLLD will get a group of users by lastLogin DESC
-func (s *Store) getUsersLLD(ctx context.Context) ([]User, error) {
+// getUsersSearchOptionsDesc will get users search with sorting desc
+func (s *Store) getUsersSearchOptionsDesc(ctx context.Context, search, sortBy string) ([]User, error) {
 	var u []User
 	err := s.db.SelectContext(ctx, &u,
-		`SELECT user_id, username, nickname, first_name, last_name, email, last_login, avatar, use_gravatar
+		`SELECT *
 		FROM people.users
-		ORDER BY last_login DESC NULLS LAST;`)
+		WHERE (CAST(user_id AS TEXT) LIKE '%' || $1 || '%')
+		   OR (username LIKE '%' || $1 || '%')
+		   OR (nickname LIKE '%' || $1 || '%')
+		   OR (first_name LIKE '%' || $1 || '%')
+		   or (last_name LIKE '%' || $1 || '%')
+		   OR (email LIKE '%' || $1 || '%')
+		   OR (first_name || ' ' || last_name LIKE '%' || $1 || '%')
+		ORDER BY
+		    CASE WHEN $2 = 'userId' THEN user_id END DESC,
+		    CASE WHEN $2 = 'name' THEN first_name END DESC,
+			CASE WHEN $2 = 'name' THEN last_name END DESC,
+		    CASE WHEN $2 = 'username' THEN username END DESC,
+		    CASE WHEN $2 = 'email' THEN email END DESC,
+		    CASE WHEN $2 = 'lastLogin' THEN last_login END DESC NULLS LAST;`, search, sortBy)
+	if err != nil {
+		return nil, err
+	}
+	return u, nil
+}
+
+// getUsersSearchOptionsDescSizePage will get users search with sorting desc with size and page
+func (s *Store) getUsersSearchOptionsDescSizePage(ctx context.Context, search, sortBy string, size, page int) ([]User, error) {
+	var u []User
+	err := s.db.SelectContext(ctx, &u,
+		`SELECT *
+		FROM people.users
+		WHERE (CAST(user_id AS TEXT) LIKE '%' || $1 || '%')
+		   OR (username LIKE '%' || $1 || '%')
+		   OR (nickname LIKE '%' || $1 || '%')
+		   OR (first_name LIKE '%' || $1 || '%')
+		   or (last_name LIKE '%' || $1 || '%')
+		   OR (email LIKE '%' || $1 || '%')
+		   OR (first_name || ' ' || last_name LIKE '%' || $1 || '%')
+		ORDER BY
+		    CASE WHEN $2 = 'userId' THEN user_id END DESC,
+		    CASE WHEN $2 = 'name' THEN first_name END DESC,
+			CASE WHEN $2 = 'name' THEN last_name END DESC,
+		    CASE WHEN $2 = 'username' THEN username END DESC,
+		    CASE WHEN $2 = 'email' THEN email END DESC,
+		    CASE WHEN $2 = 'lastLogin' THEN last_login END DESC NULLS LAST
+		LIMIT $3
+		OFFSET $4;`, search, sortBy, size, size*(page-1))
 	if err != nil {
 		return nil, err
 	}
@@ -224,8 +338,8 @@ func (s *Store) getUsersLLD(ctx context.Context) ([]User, error) {
 }
 
 // getPermissionsForUser returns all permissions for a user
-func (s *Store) getPermissionsForUser(ctx context.Context, u User) (p []permission.Permission, err error) {
-	err = s.db.SelectContext(ctx, &p, `SELECT p.permission_id, p.name, p.description
+func (s *Store) getPermissionsForUser(ctx context.Context, u User) (p []string, err error) {
+	err = s.db.SelectContext(ctx, &p, `SELECT p.name
 		FROM people.permissions p
 		INNER JOIN people.role_permissions rp ON rp.permission_id = p.permission_id
 		INNER JOIN people.role_members rm ON rm.role_id = rp.role_id
@@ -238,7 +352,7 @@ func (s *Store) getPermissionsForUser(ctx context.Context, u User) (p []permissi
 
 // getRolesForUser returns all roles for a user
 func (s *Store) getRolesForUser(ctx context.Context, u User) (r []role.Role, err error) {
-	err = s.db.SelectContext(ctx, &r, `SELECT r.role_id, r.name, r.description
+	err = s.db.SelectContext(ctx, &r, `SELECT r.*
 		FROM people.roles r
 		INNER JOIN people.role_members rm ON rm.role_id = r.role_id
 		WHERE rm.user_id = $1;`, u.UserID)
