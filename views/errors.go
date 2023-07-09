7db203a9edd1060f0dcd6646e5b98487d044ca18
Liam Burnand <liam.burnand@ystv.co.uk>
2023-07-09 10:27:16 +0100

    General update and adding 500 error

--- views/errors.go
+++ views/errors.go
@@ -1,14 +1,14 @@
 package views
 
 import (
-	"fmt"
-	"github.com/ystv/web-auth/public/templates"
-	"net/http"
+	"github.com/labstack/echo/v4"
+	"github.com/ystv/web-auth/templates"
 )
 
-func (v *Views) Error404(w http.ResponseWriter, _ *http.Request) {
-	err := v.template.RenderNoNavsTemplate(w, nil, templates.NotFound404Template)
-	if err != nil {
-		fmt.Println(err)
-	}
+func (v *Views) Error404(c echo.Context) error {
+	return v.template.RenderNoNavsTemplate(c.Response().Writer, nil, templates.NotFound404Template)
+}
+
+func (v *Views) Error500(c echo.Context) error {
+	return v.template.RenderNoNavsTemplate(c.Response().Writer, nil, templates.Forbidden500Template)
 }
