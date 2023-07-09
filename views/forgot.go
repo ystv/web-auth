2a1d650fbd0591e10091a63ed139959eb0c0b70b
Liam Burnand <liam.burnand@ystv.co.uk>
2023-07-09 10:23:21 +0100

    Move over to echo and moved reset to its own file, prefer it to be seperated.
    Also removed unnecessary things and making sure everything matches all the new files

--- views/forgot.go
+++ views/forgot.go
@@ -1,12 +1,11 @@
 package views
 
 import (
-	"fmt"
 	"github.com/google/uuid"
-	"github.com/gorilla/mux"
+	"github.com/labstack/echo/v4"
 	"github.com/patrickmn/go-cache"
 	"github.com/ystv/web-auth/infrastructure/mail"
-	"github.com/ystv/web-auth/public/templates"
+	"github.com/ystv/web-auth/templates"
 	"github.com/ystv/web-auth/user"
 	"log"
 	"net/http"
@@ -28,49 +27,34 @@ var notification = Notification{
 }
 
 // ForgotFunc handles sending a reset email
-func (v *Views) ForgotFunc(w http.ResponseWriter, r *http.Request) {
+func (v *Views) ForgotFunc(c echo.Context) error {
 	var err error
-	switch r.Method {
+	switch c.Request().Method {
 	case "GET":
-		err = v.template.RenderNoNavsTemplate(w, nil, templates.ForgotTemplate)
-		if err != nil {
-			err = fmt.Errorf("failed to exec tmpl: %w", err)
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
-		return
+		return v.template.RenderNoNavsTemplate(c.Response().Writer, nil, templates.ForgotTemplate)
 	case "POST":
-		err = r.ParseForm()
+		err = c.Request().ParseForm()
 		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
+			http.Error(c.Response(), err.Error(), http.StatusBadRequest)
 		}
-		u := user.User{Email: r.Form.Get("email")}
+		u := user.User{Email: c.Request().Form.Get("email")}
 
 		if u.Email == "" {
-			err = v.template.RenderNoNavsTemplate(w, nil, templates.ForgotTemplate)
-			if err != nil {
-				err = fmt.Errorf("failed to exec tmpl: %w", err)
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-			}
-			return
+			return v.template.RenderNoNavsTemplate(c.Response(), nil, templates.ForgotTemplate)
 		}
 		// Get user1 and check if it exists
-		user1, err := v.user.GetUser(r.Context(), u)
+		user1, err := v.user.GetUser(c.Request().Context(), u)
 		if err != nil {
-			// UserStripped doesn't exist, we'll pretend they've got an email
+			// StrippedUser doesn't exist, we'll pretend they've got an email
 			log.Printf("request for reset on unknown email \"%s\"", user1.Email)
-			err = v.template.RenderNoNavsTemplate(w, notification, templates.NotificationTemplate)
-			if err != nil {
-				err = fmt.Errorf("failed to exec template: %w", err)
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-			}
-			return
+			return v.template.RenderNoNavsTemplate(c.Response(), notification, templates.NotificationTemplate)
 		}
 		url := uuid.NewString()
 		v.cache.Set(url, user1.UserID, cache.DefaultExpiration)
 
 		// Valid request, send email with reset code
-		if v.mailer.Enabled {
-			v.mailer, err = mail.NewMailer(mail.Config{
+		if v.Mailer.Enabled {
+			v.Mailer, err = mail.NewMailer(mail.Config{
 				Host:       v.conf.Mail.Host,
 				Port:       v.conf.Mail.Port,
 				Username:   v.conf.Mail.Username,
@@ -78,12 +62,12 @@ func (v *Views) ForgotFunc(w http.ResponseWriter, r *http.Request) {
 				DomainName: v.conf.DomainName,
 			})
 			if err != nil {
-				log.Printf("mailer failed: %+v", err)
+				log.Printf("Mailer failed: %+v", err)
 			}
 
 			file := mail.Mail{
 				Subject: "YSTV Security - Reset Password",
-				Tpl:     v.template.RenderEmail(templates.ForgotEmailTemplate),
+				Tpl:     v.template.RenderEmail(templates.ForgotPasswordEmailTemplate),
 				To:      user1.Email,
 				From:    "YSTV Security <no-reply@ystv.co.uk>",
 				TplData: struct {
@@ -95,125 +79,18 @@ func (v *Views) ForgotFunc(w http.ResponseWriter, r *http.Request) {
 				},
 			}
 
-			err = v.mailer.SendMail(file)
+			err = v.Mailer.SendMail(file)
 			if err != nil {
-				err = v.errorHandle(w, err)
-				if err != nil {
-					fmt.Println(err)
-				}
+				return v.errorHandle(c, err)
 			}
-
-			//err = v.mailer.SendResetEmail(user1.Email, "Forgotten Password", code)
-			//if err != nil {
-			//	log.Printf("SendResetEmail failed: %s, ", err)
-			//	log.Printf("request for password reset email \"%s\":reset code \"%s\"", user1.Email, code)
-			//}
 			log.Printf("request for password reset email: \"%s\"", user1.Email)
 		} else {
-			log.Printf("no mailer present")
+			log.Printf("no Mailer present")
 			log.Printf("reset email: %s, code: %s, reset link: https://%s/reset?code=%s", user1.Email, url, v.conf.DomainName, url)
 		}
 
-		// UserStripped doesn't exist, we'll pretend they've got an email
-		err = v.template.RenderNoNavsTemplate(w, notification, templates.NotificationTemplate)
-		//err = v.tpl.ExecuteTemplate(w, "notification.tmpl", notification)
-		if err != nil {
-			err = fmt.Errorf("failed to exec template: %w", err)
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-	}
-}
-
-func (v *Views) ResetURLFunc(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	url1 := vars["url"]
-
-	userID, found := v.cache.Get(url1)
-	if !found {
-		err := v.errorHandle(w, fmt.Errorf("failed to get url"))
-		if err != nil {
-			fmt.Println(err)
-		}
-		return
-	}
-
-	user1, err := v.user.GetUser(r.Context(), user.User{UserID: userID.(int)})
-	if err != nil {
-		v.cache.Delete(url1)
-		err = v.errorHandle(w, fmt.Errorf("url is invalid because this user doesn't exist"))
-		if err != nil {
-			fmt.Println(err)
-		}
-		return
-	}
-
-	switch r.Method {
-	case "GET":
-		err = v.template.RenderNoNavsTemplate(w, nil, templates.ResetTemplate)
-		if err != nil {
-			err = v.errorHandle(w, err)
-			if err != nil {
-				fmt.Println(err)
-			}
-			return
-		}
-		return
-	case "POST":
-		err = r.ParseForm()
-		if err != nil {
-			err = v.errorHandle(w, err)
-			if err != nil {
-				fmt.Println(err)
-			}
-		}
-
-		password := r.FormValue("password")
-		if password != r.FormValue("confirmpassword") {
-			err = v.template.RenderNoNavsTemplate(w, nil, templates.ResetTemplate)
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-		}
-
-		//fmt.Println("DEBUG - RESET POST")
-		err = r.ParseForm()
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-		}
-		p := r.Form.Get("password")
-		if p != r.Form.Get("confirmpassword") || p == "" {
-			err = v.template.RenderNoNavsTemplate(w, nil, templates.ResetTemplate)
-			//err = v.tpl.ExecuteTemplate(w, "reset.tmpl", ctx)
-			if err != nil {
-				return
-			}
-			return
-		}
-		user1.Password = password
-
-		user2, err := v.user.UpdateUserPassword(r.Context(), user1)
-		if err != nil {
-			log.Printf("failed to reset user: %+v", err)
-		}
-		v.cache.Delete(url1)
-		log.Printf("updated user: %s", user2.Username)
-		http.Redirect(w, r, "/", http.StatusFound)
-	}
-}
-
-func (v *Views) errorHandle(w http.ResponseWriter, err error) error {
-	data := struct {
-		Error string
-	}{
-		Error: err.Error(),
-	}
-	fmt.Println(data.Error)
-	err = v.template.RenderNoNavsTemplate(w, data, templates.ErrorTemplate)
-	if err != nil {
-		fmt.Println(err)
-		return err
+		// StrippedUser doesn't exist, we'll pretend they've got an email
+		return v.template.RenderNoNavsTemplate(c.Response().Writer, notification, templates.NotificationTemplate)
 	}
 	return nil
 }
---
+++ views/reset.go
@@ -0,0 +1,155 @@
+package views
+
+import (
+	"fmt"
+	"github.com/google/uuid"
+	"github.com/labstack/echo/v4"
+	"github.com/patrickmn/go-cache"
+	"github.com/ystv/web-auth/infrastructure/mail"
+	"github.com/ystv/web-auth/templates"
+	"github.com/ystv/web-auth/user"
+	"gopkg.in/guregu/null.v4"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+func (v *Views) ResetURLFunc(c echo.Context) error {
+	url := c.Param("url")
+
+	userID, found := v.cache.Get(url)
+	if !found {
+		return v.errorHandle(c, fmt.Errorf("failed to get url"))
+		//if err != nil {
+		//	fmt.Println(err)
+		//}
+		//return
+	}
+
+	user1, err := v.user.GetUser(c.Request().Context(), user.User{UserID: userID.(int)})
+	if err != nil {
+		v.cache.Delete(url)
+		return v.errorHandle(c, fmt.Errorf("url is invalid because this user doesn't exist"))
+	}
+
+	switch c.Request().Method {
+	case "GET":
+		return v.template.RenderNoNavsTemplate(c.Response(), nil, templates.ResetTemplate)
+	case "POST":
+		err = c.Request().ParseForm()
+		if err != nil {
+			return v.errorHandle(c, err)
+			//if err != nil {
+			//	fmt.Println(err)
+			//}
+		}
+
+		password := c.Request().FormValue("password")
+		if password != c.Request().FormValue("confirmpassword") {
+			return v.template.RenderNoNavsTemplate(c.Response(), nil, templates.ResetTemplate)
+		}
+
+		err = c.Request().ParseForm()
+		if err != nil {
+			http.Error(c.Response(), err.Error(), http.StatusBadRequest)
+		}
+		p := c.Request().Form.Get("password")
+		if p != c.Request().Form.Get("confirmpassword") || p == "" {
+			return v.template.RenderNoNavsTemplate(c.Response(), nil, templates.ResetTemplate)
+		}
+		user1.Password = null.StringFrom(password)
+
+		user2, err := v.user.UpdateUserPassword(c.Request().Context(), user1)
+		if err != nil {
+			log.Printf("failed to reset user: %+v", err)
+		}
+		v.cache.Delete(url)
+		log.Printf("updated user: %s", user2.Username)
+		return c.Redirect(http.StatusFound, "/")
+	}
+	return nil
+}
+
+func (v *Views) ResetUserPasswordFunc(c echo.Context) error {
+	session, _ := v.cookie.Get(c.Request(), v.conf.SessionCookieName)
+
+	c1 := v.getData(session)
+
+	userID, err := strconv.Atoi(c.Param("userid"))
+	if err != nil {
+		return v.errorHandle(c, err)
+		//if err != nil {
+		//	fmt.Println(err)
+		//}
+		//return
+	}
+
+	user1, err := v.user.GetUser(c.Request().Context(), user.User{UserID: userID})
+	if err != nil {
+		log.Println(err)
+		return v.errorHandle(c, err)
+	}
+
+	user1.ResetPw = true
+
+	_, err = v.user.UpdateUser(c.Request().Context(), user1, c1.User.UserID)
+	if err != nil {
+		return v.errorHandle(c, err)
+	}
+
+	url := uuid.NewString()
+	v.cache.Set(url, user1.UserID, cache.DefaultExpiration)
+
+	var status int
+
+	var message struct {
+		Message string `json:"message"`
+		Error   error  `json:"error"`
+	}
+
+	// Valid request, send email with reset code
+	if v.Mailer.Enabled {
+		v.Mailer, err = mail.NewMailer(mail.Config{
+			Host:       v.conf.Mail.Host,
+			Port:       v.conf.Mail.Port,
+			Username:   v.conf.Mail.Username,
+			Password:   v.conf.Mail.Password,
+			DomainName: v.conf.DomainName,
+		})
+		if err != nil {
+			log.Printf("Mailer failed: %+v", err)
+		}
+
+		file := mail.Mail{
+			Subject: "YSTV Security - Reset Password",
+			Tpl:     v.template.RenderEmail(templates.ResetPasswordEmailTemplate),
+			To:      user1.Email,
+			From:    "YSTV Security <no-reply@ystv.co.uk>",
+			TplData: struct {
+				Email string
+				URL   string
+			}{
+				Email: user1.Email,
+				URL:   fmt.Sprintf("https://%s/reset/%s", v.conf.DomainName, url),
+			},
+		}
+
+		err = v.Mailer.SendMail(file)
+		if err != nil {
+			return v.errorHandle(c, err)
+			//if err != nil {
+			//	fmt.Println(err)
+			//}
+		}
+
+		log.Printf("request for password reset email: \"%s\"", user1.Email)
+		message.Message = fmt.Sprintf("Reset email sent to: \"%s\"", user1.Email)
+	} else {
+		message.Message = fmt.Sprintf("No mailer present\nPlease forward the link to this email: %s, reset link: https://%s/reset/%s", user1.Email, v.conf.DomainName, url)
+		message.Error = fmt.Errorf("no mailer present")
+		log.Printf("no Mailer present")
+		log.Printf("reset email: %s, url: %s, reset link: https://%s/reset/%s", user1.Email, url, v.conf.DomainName, url)
+	}
+	log.Printf("reset for %d (%s) requested by %d (%s)", user1.UserID, user1.Firstname+" "+user1.Lastname, c1.User.UserID, c1.User.Firstname+" "+c1.User.Lastname)
+	return c.JSON(status, message)
+}
