03246e3ff5e6252ecf61dbeddda6e845242b9af8
Liam Burnand <liam.burnand@ystv.co.uk>
2023-08-06 13:53:47 +0100

    Users can change some details and password now, plus the pointer wasn't needed and can be a bit of an issue sometimes

--- views/settings.go
+++ views/settings.go
@@ -8,13 +8,15 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/ystv/web-auth/templates"
 	"github.com/ystv/web-auth/user"
+	"log"
+	"net/http"
 	"strings"
 	"time"
 )
 
 type (
 	SettingsTemplate struct {
-		User       *user.User
+		User       user.User
 		UserID     int
 		LastLogin  string
 		ActivePage string
@@ -27,6 +29,50 @@ func (v *Views) SettingsFunc(c echo.Context) error {
 	session, _ := v.cookie.Get(c.Request(), v.conf.SessionCookieName)
 
 	c1 := v.getData(session)
+
+	if c.Request().Method == http.MethodPost {
+		err := c.Request().ParseForm()
+		if err != nil {
+			return v.errorHandle(c, fmt.Errorf("failed to parse form for settings: %+v", err))
+		}
+
+		firstName := c.Request().FormValue("firstname")
+		nickname := c.Request().FormValue("nickname")
+		lastName := c.Request().FormValue("lastname")
+		// avatar type can't be changed yet but the infrastructure is in
+		avatar := c.Request().FormValue("avatar")
+		_ = avatar
+
+		if firstName != c1.User.Firstname && len(firstName) > 0 {
+			c1.User.Firstname = firstName
+		}
+		if nickname != c1.User.Nickname && len(nickname) > 0 {
+			c1.User.Nickname = nickname
+		}
+		if lastName != c1.User.Lastname && len(lastName) > 0 {
+			c1.User.Lastname = lastName
+		}
+
+		_, err = v.user.EditUser(c.Request().Context(), c1.User, c1.User.UserID)
+		if err != nil {
+			log.Printf("failed to edit user for settings: %+v", err)
+			if !v.conf.Debug {
+				return v.errorHandle(c, fmt.Errorf("failed to edit user for settings: %+v", err))
+			}
+		}
+
+		session.Values["user"] = c1.User
+
+		err = session.Save(c.Request(), c.Response())
+		if err != nil {
+			err = fmt.Errorf("failed to save user session in settings: %w", err)
+			return v.errorHandle(c, err)
+		}
+
+		c.Request().Method = http.MethodGet
+		return v.SettingsFunc(c)
+	}
+
 	lastLogin := time.Now()
 	if c1.User.LastLogin.Valid {
 		lastLogin = c1.User.LastLogin.Time
