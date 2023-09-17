74901a19d5364cadebd96c688d8d4662a27fa5ef
Liam Burnand <liam.burnand@ystv.co.uk>
2023-09-17 20:10:08 +0100

    Fixing from math/rand to crypto/rand

--- utils/hash.go
+++ utils/hash.go
@@ -1,10 +1,11 @@
 package utils
 
 import (
+	"crypto/rand"
 	"encoding/hex"
-	mRand "math/rand"
-
 	whirl "github.com/balacode/zr-whirl"
+	"log"
+	"math/big"
 )
 
 const (
@@ -27,20 +28,30 @@ func HashPass(password string) string {
 
 // GenerateSalt generates a salt for the password to be salted against
 func GenerateSalt() string {
+	lenSalt := big.NewInt(int64(len(SaltCharacters)))
+
 	b := make([]byte, 22)
 	for i := range b {
-		// #nosec
-		b[i] = SaltCharacters[mRand.Intn(len(SaltCharacters))]
+		randInt, err := rand.Int(rand.Reader, lenSalt)
+		if err != nil {
+			log.Println("Error generating random salt:", err)
+		}
+		b[i] = SaltCharacters[randInt.Int64()]
 	}
 	return "$2a$06$" + string(b)
 }
 
 // GeneratePassword generates a random password for a user to change
 func GeneratePassword() string {
+	lenPass := big.NewInt(int64(len(PasswordCharacters)))
+
 	b := make([]byte, 12)
 	for i := range b {
-		// #nosec
-		b[i] = PasswordCharacters[mRand.Intn(len(PasswordCharacters))]
+		randInt, err := rand.Int(rand.Reader, lenPass)
+		if err != nil {
+			log.Println("Error generating random password:", err)
+		}
+		b[i] = PasswordCharacters[randInt.Int64()]
 	}
 	return string(b)
 }
