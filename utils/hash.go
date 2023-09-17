6ca86c2235be919ac86d5166654e4a40989bb706
Liam Burnand <liam.burnand@ystv.co.uk>
2023-09-17 20:04:36 +0100

    Upgrading from math/rand to crypto/rand

--- utils/hash.go
+++ utils/hash.go
@@ -1,9 +1,12 @@
 package utils
 
 import (
+	"crypto/rand"
 	"encoding/hex"
+	"log"
+	"math/big"
+
 	whirl "github.com/balacode/zr-whirl"
-	mRand "math/rand"
 )
 
 const (
@@ -25,17 +28,29 @@ func HashPass(password string) string {
 }
 
 func GenerateSalt() string {
+	lenSalt := big.NewInt(int64(len(SaltCharacters)))
+
 	b := make([]byte, 22)
 	for i := range b {
-		b[i] = SaltCharacters[mRand.Intn(len(SaltCharacters))]
+		randInt, err := rand.Int(rand.Reader, lenSalt)
+		if err != nil {
+			log.Println("Error generating random salt:", err)
+		}
+		b[i] = SaltCharacters[randInt.Int64()]
 	}
 	return "$2a$06$" + string(b)
 }
 
 func GeneratePassword() string {
+	lenPass := big.NewInt(int64(len(PasswordCharacters)))
+
 	b := make([]byte, 12)
 	for i := range b {
-		b[i] = PasswordCharacters[mRand.Intn(len(PasswordCharacters))]
+		randInt, err := rand.Int(rand.Reader, lenPass)
+		if err != nil {
+			log.Println("Error generating random password:", err)
+		}
+		b[i] = PasswordCharacters[randInt.Int64()]
 	}
 	return string(b)
 }
