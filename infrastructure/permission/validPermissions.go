909bb7e0b00dc01e43260b84bf99abcb3585b7a9
Liam Burnand <liam.burnand@ystv.co.uk>
2024-02-05 23:41:06 +0000

    Allowing fallthrough

--- infrastructure/permission/validPermissions.go
+++ infrastructure/permission/validPermissions.go
@@ -13,43 +13,60 @@ func SufficientPermissionsFor(p permissions.Permissions) (m map[string]bool) {
 		m[permissions.MenuDisabled.String()] = true
 		return m
 	case permissions.ManageMembersMembersList:
+		fallthrough
 	case permissions.ManageMembersMembersAdd:
 		m[permissions.ManageMembersMembersAdmin.String()] = true
+		fallthrough
 	case permissions.ManageMembersPermissions:
+		fallthrough
 	case permissions.ManageMembersMicsKeyList:
+		fallthrough
 	case permissions.ManageMembersMiscUnpaidList:
+		fallthrough
 	case permissions.ManageMembersOfficers:
+		fallthrough
 	case permissions.ManageMembersGroup:
+		fallthrough
 	case permissions.ManageMembersMembersAdmin:
 		m[permissions.ManageMembersAdmin.String()] = true
 		break
 	case permissions.EmailAccess:
+		fallthrough
 	case permissions.EmailAlumni:
+		fallthrough
 	case permissions.EmailOfficers:
 		m[permissions.EmailEveryone.String()] = true
 		break
 	case permissions.CalendarSocialCreator:
 		m[permissions.CalendarSocialAdmin.String()] = true
+		fallthrough
 	case permissions.CalendarSocialAdmin:
 		m[permissions.CalendarAdmin.String()] = true
 		break
 	case permissions.CalendarShowCreator:
 		m[permissions.CalendarShowAdmin.String()] = true
+		fallthrough
 	case permissions.CalendarShowAdmin:
 		m[permissions.CalendarAdmin.String()] = true
 		break
 	case permissions.CalendarMeetingCreator:
 		m[permissions.CalendarMeetingAdmin.String()] = true
+		fallthrough
 	case permissions.CalendarMeetingAdmin:
 		m[permissions.CalendarAdmin.String()] = true
 		break
 	case permissions.CMSNewsItemCreator:
 		m[permissions.CMSNewsItemAdmin.String()] = true
+		fallthrough
 	case permissions.CMSNewsItemAdmin:
 		m[permissions.CMSNewsAdmin.String()] = true
+		fallthrough
 	case permissions.CMSEndboardAdmin:
+		fallthrough
 	case permissions.CMSView:
+		fallthrough
 	case permissions.CMSPermalinkAdmin:
+		fallthrough
 	case permissions.CMSNewsAdmin:
 		m[permissions.CMSAdmin.String()] = true
 		break
@@ -59,11 +76,13 @@ func SufficientPermissionsFor(p permissions.Permissions) (m map[string]bool) {
 		break
 	case permissions.CMSPageCreator:
 		m[permissions.CMSPageAdmin.String()] = true
+		fallthrough
 	case permissions.CMSPageAdmin:
 		m[permissions.CMSAdmin.String()] = true
 		break
 	case permissions.CMSSlideshowCreator:
 		m[permissions.CMSSlideshowAdmin.String()] = true
+		fallthrough
 	case permissions.CMSSlideshowAdmin:
 		m[permissions.CMSAdmin.String()] = true
 		break
