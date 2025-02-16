e3ac5ce063255236b631cf1e6f4eeaca805263a6
Liam Burnand <liam.burnand@ystv.co.uk>
2025-02-16 21:03:53 +0000

    Updating the mocks

--- user/mocks/mock_user.go
+++ user/mocks/mock_user.go
@@ -131,6 +131,20 @@ func (mr *MockRepoMockRecorder) EditUser(arg0, arg1, arg2 any) *gomock.Call {
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "EditUser", reflect.TypeOf((*MockRepo)(nil).EditUser), arg0, arg1, arg2)
 }
 
+// EditUserAvatar mocks base method.
+func (m *MockRepo) EditUserAvatar(arg0 context.Context, arg1 user.User) error {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "EditUserAvatar", arg0, arg1)
+	ret0, _ := ret[0].(error)
+	return ret0
+}
+
+// EditUserAvatar indicates an expected call of EditUserAvatar.
+func (mr *MockRepoMockRecorder) EditUserAvatar(arg0, arg1 any) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "EditUserAvatar", reflect.TypeOf((*MockRepo)(nil).EditUserAvatar), arg0, arg1)
+}
+
 // EditUserPassword mocks base method.
 func (m *MockRepo) EditUserPassword(arg0 context.Context, arg1 user.User) error {
 	m.ctrl.T.Helper()
